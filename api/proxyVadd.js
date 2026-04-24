3254c640959f615c7d2f6231f7d8f7cb2e8c4969
gggg1 <urmaybe99@gmail.com>
2026-04-24 16:32:30 -0400

    Update proxyVadd.js

--- api/proxyVadd.js
+++ api/proxyVadd.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  // Your Google Apps Script URL goes here
+
   const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby6lG_wVwBhNLb6N8R8-7N624PM_N7DrwiUTxl9Jf6h9DGmLPJmbjFZZW23UXKgA1yM_g/exec";
 
   try {
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(req.body),
-      redirect: "follow" // This is what fixes your redirect problem!
+      redirect: "follow" 
     });
 
     const text = await response.text();
