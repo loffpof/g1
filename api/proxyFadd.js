c5d49e80601ce98eca8662aebad26e763e879efe
gggg1 <urmaybe99@gmail.com>
2026-04-25 19:01:14 -0400

    Create proxyFadd.js

---
+++ api/proxyFadd.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxfVWA_PuF-sjD1rvKS3hRiJLcpucDo8TAGwREsVzeWCVyaQYM5h07BE--F3PGpdKmMMA/exec";
+
+  try {
+    const response = await fetch(GOOGLE_SCRIPT_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(req.body),
+      redirect: "follow" 
+    });
+
+    const text = await response.text();
+    res.status(200).send(text);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
