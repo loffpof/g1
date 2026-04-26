9fb39a0d16f4404c1a904990171801e6e394279a
gggg1 <urmaybe99@gmail.com>
2026-04-25 23:55:51 -0400

    Create proxyFremove.js

---
+++ api/proxyFremove.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzmQeplHFC-FTmjUf1d12wqA4D1nlK6y2V8PkVO4A6O8soYrReN5c6Z4IUYehIvZ7_Omg/exec";
+
+  try {
+    const response = await fetch(GOOGLE_SCRIPT_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(req.body),
+      redirect: "follow" 
+    });
+
+    const text = await response.text();
+    res.status(200).send(text);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
