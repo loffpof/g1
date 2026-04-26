057b5a4a8f765ccdbca2d0b9327e1e45f19044b8
gggg1 <urmaybe99@gmail.com>
2026-04-26 03:25:20 -0400

    Create proxycaptainchange.js

---
+++ api/proxycaptainchange.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxoXdXFSwDrArVjnERudf8dMSRHge08rt7koSGo59wgUdGSvrIs99yhJLAy_v3z3PwJrw/exec";
+
+  try {
+    const response = await fetch(GOOGLE_SCRIPT_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(req.body),
+      redirect: "follow" 
+    });
+
+    const text = await response.text();
+    res.status(200).send(text);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
