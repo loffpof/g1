765257cadebeeb007d435076422f44113a3d3217
gggg1 <urmaybe99@gmail.com>
2026-04-30 20:10:01 -0400

    Update proxyupdate.js

--- api/proxyupdate.js
+++ api/proxyupdate.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxtzb4pJIAeR3gyjY6TEW-08D1P8XFiLnb9kPff7mZtKCtnoJ-b7bMg9FCNRbPMYzJYGw/exec";
+  const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwMy7K_HQiirqCtqsgdfVf-HeYMTZk5EZiNlM45O83H4412V2PDpbfEGdq6QKyKJS1m_w/exec";
 
   try {
     const response = await fetch(GOOGLE_SCRIPT_URL, {
